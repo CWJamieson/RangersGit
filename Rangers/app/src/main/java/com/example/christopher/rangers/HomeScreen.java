c5479ddc6aad0f43a05a1997c66e11a821e3b0dd
Christopher <christopherjohnjamieson@hotmail.com>
2016-12-17 01:02:59 -0500

    Changed camera to input

--- Rangers/app/src/main/java/com/example/christopher/rangers/HomeScreen.java
+++ Rangers/app/src/main/java/com/example/christopher/rangers/HomeScreen.java
@@ -80,8 +80,8 @@ public class HomeScreen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_input) {
+            getInput();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -97,5 +97,9 @@ public class HomeScreen extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    private void getInput()
+    {
+        
     }
 }
